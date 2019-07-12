bf194d9693270b11f63a48ec69a57a9504184b26
Zedo9899 <dechly98@gmail.com>
2019-07-13 00:04:10 +0100

    try 2

--- Modules/Commands.js
+++ Modules/Commands.js
@@ -68,7 +68,7 @@ mp.events.addCommand('dc', (player) => {
 mp.events.addCommand("weapon", (player, fullText, weapon, ammo) => {
 	let weaponHash = mp.joaat(weapon);
     player.giveWeapon(weaponHash, parseInt(ammo) || 10000)
-    player.outputChatBox('You were given $(ammo) of $(weapon)');
+    player.outputChatBox('You were given ' + toString(ammo) +' of '+ toString(weapon));
 });
 /*
 mp.events.addCommand('giveweapon', (player, fullText, abc, bcd) => {
