19dec5e2668e31778b634687e14ca30df63c3b07
smartxma23 <smart.ma23@gmail.com>
2019-07-14 12:52:38 +0100

    pos change pd blip

--- Modules/pdcommands.js
+++ Modules/pdcommands.js
@@ -1,4 +1,4 @@
-let policeBlip = mp.blips.new(60, new mp.Vector3(427.95, -981.05, 0),
+let policeBlip = mp.blips.new(60, new mp.Vector3(428, -979, 0),
     {
         name: 'Los Santos Police Station',
         color: 3,
