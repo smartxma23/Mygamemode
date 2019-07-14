9dfbe0fff6fd17de9b22b8ea0d699ebbd59046cc
smartxma23 <smart.ma23@gmail.com>
2019-07-14 01:16:57 +0100

    z fix2

--- Modules/mdcommands.js
+++ Modules/mdcommands.js
@@ -1,8 +1,8 @@
 function generateColShapes() {
-    var specialColshape = mp.colshapes.newSphere(329, -558, 20.3, 3);
+    var specialColshape = mp.colshapes.newSphere(329, -558, 25.3, 3);
     specialColshape.setVariable('PersonalVehicle', 'Ambulance');
-    var specialColshapeText = mp.labels.new('Ambulance', new mp.Vector3(329, -558, 20.3), { drawDistance: 10 });
-    var specialColShapeMarker = mp.markers.new(1, new mp.Vector3(329, -558, 20.3), 3, { color: [255, 255, 255, 100], visible: true});
+    var specialColshapeText = mp.labels.new('Ambulance', new mp.Vector3(329, -558, 25.3), { drawDistance: 10 });
+    var specialColShapeMarker = mp.markers.new(1, new mp.Vector3(329, -558, 25.3), 3, { color: [255, 255, 255, 100], visible: true});
 };
 
 generateColShapes();
